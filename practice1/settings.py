daaa96f359b5be0aeb21f738bb438cdb4017d7de
EC2 Default User <ec2-user@ip-172-31-95-144.ec2.internal>
2019-08-15 03:17:16 +0000

    modified settings.py

--- practice1/settings.py
+++ practice1/settings.py
@@ -176,7 +176,9 @@ USE_L10N = True
 USE_TZ = True
 
 CORS_ORIGIN_ALLOW_ALL = True # 追加
-
+CORS_ORIGIN_WHITELIST = (
+    'https://practice.lolipop.io',
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
